57deb5cf466540ec5917a70c4d255fc953380b15
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-11-22 16:41:07 -0600

    tried swtiching up order of routes

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,41 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([{
+        $addField: {
+            totalDuration: {
+                $sum: '$exercises.duration',
+            },
+        },
+    },]
+        .sort({ _id: -1 })
+        .limit(7)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        })
+    )
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+            },
+        },
+    ])
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
 router.put("/api/workouts/:id", ({ body, params }, res) => {
     Workout.findByIdAndUpdate(
         params.id,
@@ -40,50 +75,14 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
         });
 });
 
-router.get("/api/workouts/range", (req, res) => {
-    Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: { $sum: "$exercises.duration" },
-            },
-        },
-    ])
-        .then((workouts) => {
-            res.json(workouts);
-        })
-        .catch((err) => {
-            res.status(400).json(err);
-        });
-});
-
-
-router.get("/api/workouts", (req, res) => {
-    Workout.aggregate([{
-        $addField: {
-            totalDuration: {
-                $sum: '$exercises.duration',
-            },
-        },
-    },]
-        .sort({ _id: -1 })
-        .limit(7)
-        .then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-        .catch((err) => {
-            res.json(err);
-        })
-    )
-});
-
-router.delete("/api/workouts", ({ body }, res) => {
-    Workout.findByIdAndDelete(body.id)
-        .then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
-});
+// router.delete("/api/workouts", ({ body }, res) => {
+//     Workout.findByIdAndDelete(body.id)
+//         .then((dbWorkout) => {
+//             res.json(dbWorkout);
+//         })
+//         .catch((err) => {
+//             res.json(err);
+//         });
+// });
 
 module.exports = router;
